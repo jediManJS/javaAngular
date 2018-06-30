eb6f69194b4e837f06e8dea6b7cf0395354cc7f0
alexDA14 <alexey.strelnikov@da-14.com>
2018-06-30 18:36:34 +0300

    login form changes
    Login form and service changes

--- src/app/api/login-registration.service.ts
+++ src/app/api/login-registration.service.ts
@@ -2,9 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable()
 export class LoginRegistrationService {
   constructor(private http: HttpClient) {
   }
+  private apiURL = environment.apiUri;
 
+  public loginUser(params): Observable<any> {
+    return this.http.post(`${this.apiURL}/login`, { params });
+  }
 }
--- src/app/login-registration/login-registration.component.ts
+++ src/app/login-registration/login-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginRegistrationService } from '../api/login-registration.service';
 
 @Component({
   selector: 'jav-login-registration',
@@ -10,7 +11,8 @@ export class LoginRegistrationComponent implements OnInit {
   public loginForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private loginRegisterService: LoginRegistrationService
   ) {
     this.createLoginForm();
   }
@@ -20,13 +22,24 @@ export class LoginRegistrationComponent implements OnInit {
 
   private createLoginForm(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(6)]
+      email: ['', [ Validators.required, Validators.email ]],
+      password: ['', [ Validators.required, Validators.minLength(6) ]]
     });
   }
 
   public sendLoginForm(): void {
-    console.log(this.loginForm.value);
+    if (!this.loginForm.valid) {
+      return;
+    }
+    const loginParams = {
+      email: this.loginForm.get('email').value,
+      password: this.loginForm.get('password').value
+    };
+    this.loginRegisterService.loginUser(loginParams)
+      .subscribe(result => {
+        console.log(result);
+        localStorage.setItem('accessToken', result.token);
+      });
   }
 
   public resetLoginForm(): void {
